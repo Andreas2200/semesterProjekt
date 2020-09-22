9819a9c70f8dd4b75c9c465d85c525524380e695
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-09-22 09:09:35 +0200

    Updated main.java

--- Kode/src/com/company/Main.java
+++ Kode/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Mojn!!!");
+        System.out.println("Hejas");
         System.out.println("Andreas er mega sej");
         System.out.println(3+3);
     }
