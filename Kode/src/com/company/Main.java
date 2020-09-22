83010b232c0367e523e961ac2ef47fa22796eed9
sivin20 <71690233+sivin20@users.noreply.github.com>
2020-09-22 09:05:09 +0200

    yoyoyo

--- Kode/src/com/company/Main.java
+++ Kode/src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Mojn!!!");
         System.out.println("Andreas er mega sej");
+        System.out.println(3+3);
     }
 }
