24511337e2ad9faef997b7d72d88283bb811174a
sivin20 <71690233+sivin20@users.noreply.github.com>
2020-09-22 18:31:15 +0200

    Ændret tekst

--- Kode/src/com/company/Main.java
+++ Kode/src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hejas");
-        System.out.println("Andreas er mega sej");
+        System.out.println("Hejsa");
+        System.out.println("Stram op, drenge!");
         System.out.println(3+3);
     }
 }
