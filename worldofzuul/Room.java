fb159326ec420d9fe2ff6015d55ec5e8678fd062
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-09-29 16:14:59 +0200

    Sync push

--- worldofzuul/Room.java
+++ worldofzuul/Room.java
@@ -10,7 +10,7 @@ public class Room
     private String description;
     private HashMap<String, Room> exits;
 
-    public Room(String description) 
+    public Room(String description)
     {
         this.description = description;
         exits = new HashMap<String, Room>();
