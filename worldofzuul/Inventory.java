0d75dd66f27ba9ef6264489a271e628e7f4bcb00
Daniel Høgh <dahoe20@student.sdu.dk>
2020-11-01 17:39:30 +0100

    Inventarsystem completed
    Uden commands.

--- worldofzuul/Inventory.java
+++ worldofzuul/Inventory.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Inventory {
+    
+    private int maxSize = 10;
+    private int currentSize = 0;
+    
+    private ArrayList<Item> items = new ArrayList<Item>(maxSize);
+    //maxSize sættes til 10, og der laves en arraylist istedet for et array.
+    //Arraylist manager det normale array for os.
+
+    public void addItem (Item item){
+        if (currentSize + item.getSize() > maxSize){
+            return;
+    //Når størrelsen af items>mængden af maxitems returneres ingenting(Går ud af metoden).
+        }
+        
+        currentSize += item.getSize();
+        items.add(item);
+    //Vi laver en additem metode, som kan tilføje items til inventaret.
+    }
+
+    public void removeItem (int index){
+        Item removedItem = items.remove(index);
+        currentSize -= removedItem.getSize();
+    //Vi laver en removeitem metode, som kan fjerne items fra inventaret.
+    }
+
+    public Item getItem (int index){
+        return items.get(index);
+    //Tager et item fra vores inventar og returnerer det.
+    }
 }
