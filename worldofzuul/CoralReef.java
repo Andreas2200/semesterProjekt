16cacb2bb6573779a65a821cf70683ad4f272e4a
pandr20 <pandr20@student.sdu.dk>
2020-11-08 18:23:45 +0100

    Pushed endgame

---
+++ worldofzuul/CoralReef.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class CoralReef {
+
+    private static Object PointSystem;
+    String endGame;
+
+
+    public static void main(String[] args) {
+//System objects
+        Scanner in = new Scanner(System.in); // Create a scanner object
+String endGame = Point;
+        //game variables
+
+        System.out.println("\tDo you want to check the condition of the coral reef by ending the game?" + "\n\ttype yes or no"); // End game ensuring for the gamer
+        System.out.println("\t1. to say yes");
+        System.out.println("\t2. to say no");
+
+        String input = in.nextLine();
+if(input.equals("1")){
+    
+}
+else if (input.equals("2")){
+
+
+    }
+else {
+
+}
+}
+
