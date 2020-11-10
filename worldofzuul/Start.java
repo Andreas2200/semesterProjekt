38b803760ff5cd5884b9b901581d94745cafaae6
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-11-10 10:48:34 +0100

    Made music play correctly

--- worldofzuul/Start.java
+++ worldofzuul/Start.java
@@ -2,7 +2,7 @@
 public class Start {
     public static void main (String[] args) {
 
-        String filepath = "Music/smashmouthallstar.wav";
+        String filepath = "MusicFileVictor.wav";
 
         PlayMusic musicObject = new PlayMusic();
         musicObject.playMusic(filepath);
