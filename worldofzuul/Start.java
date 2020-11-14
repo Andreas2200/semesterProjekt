d3e0698ee37ae7050d3894d2f96fc02a3ac0bbd2
sivin20 <71690233+sivin20@users.noreply.github.com>
2020-11-14 12:23:11 +0100

    Edited map to reflect actual pathways through map

--- worldofzuul/Start.java
+++ worldofzuul/Start.java
@@ -1,6 +1,12 @@
 
 public class Start {
     public static void main (String[] args) {
+
+        String filepath = "worldofzuul/MusicFileVictor.wav";
+
+        PlayMusic musicObject = new PlayMusic();
+        musicObject.playMusic(filepath);
+
         Game game = new Game();
         game.play();
     }
