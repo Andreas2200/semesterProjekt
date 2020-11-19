8b82eee0c64129b4c0659cadbcd3f2a6dbbe1a4a
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-11-20 00:57:52 +0100

    Imported packages from CLI version and integrated room class
    I've imported the packages from the CLI version of our game and integrated the use of rooms and added boundaries so you can't move on the outer edge of the map.
    
    Next implementation will be adding the ability to use an exit.

---
+++ Oceania/src/Room/Room.java
@@ -0,0 +1,287 @@
+package Room;
+
+import InventorySystem.*;
+import TaskSystem.*;
+import NPC.*;
+import javafx.scene.image.Image;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.HashMap;
+
+
+
+public class Room 
+{
+    private String description;
+    private HashMap<String, Room> exits;
+    private HashMap<String, Task> tasksInRoom;
+    private HashMap<String, Item> itemsInRoom;
+    private HashMap<String, NPC> NPCsInRoom;
+    private int[][] boundaries;
+    private int[][] exitLocations;
+    private int exitCounter = 0;
+    private Image roomImage;
+
+    public Room(String description, int exitLocations)
+    {
+        this.description = description;
+        exits = new HashMap<String, Room>();
+        tasksInRoom = new HashMap<String, Task>();
+        itemsInRoom = new HashMap<String, Item>();
+        NPCsInRoom = new HashMap<String, NPC>();
+        this.exitLocations = new int[exitLocations*2][2];
+        boundaries = new int[37][2];
+        setBoundaries();
+    }
+
+    private void setBoundaries()
+    {
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < boundaries.length; i++)
+        {
+            if(i < 9 && x < 1728 && y <= 0)
+            {
+                for (int j = 0; j < boundaries[i].length; j++)
+                {
+                    if(j == 0)
+                    {
+                        boundaries[i][j] = x;
+                    }
+                    else if(j == 1)
+                    {
+                        boundaries[i][j] = y;
+                    }
+                }
+                x += 192;
+            }
+            if(i > 8 && i < 18 && x == 1728 && y < 972)
+            {
+                for (int j = 0; j < boundaries[i].length; j++)
+                {
+                    if(j == 0)
+                    {
+                        boundaries[i][j] = x;
+                    }
+                    else if(j == 1)
+                    {
+                        boundaries[i][j] = y;
+                    }
+                }
+                y += 108;
+            }
+            if(i > 18 && i < 28 && x >= 0 && y == 972)
+            {
+                for (int j = 0; j < boundaries[i].length; j++)
+                {
+                    if(j == 0)
+                    {
+                        boundaries[i][j] = x;
+                    }
+                    else if(j == 1)
+                    {
+                        boundaries[i][j] = y;
+                    }
+                }
+                x -= 192;
+            }
+            if(i >= 28 && x == 0 && y >= 0)
+            {
+                for (int j = 0; j < boundaries[i].length; j++)
+                {
+                    if(j == 0)
+                    {
+                        boundaries[i][j] = x;
+                    }
+                    else if(j == 1)
+                    {
+                        boundaries[i][j] = y;
+                    }
+                }
+                y -= 108;
+            }
+        }
+    }
+
+    public void setExit(String direction, Room neighbor)
+    {
+        exits.put(direction, neighbor);
+    }
+
+    public String getShortDescription()
+    {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
+    public String getLongDescription()
+    {
+        return "You are " + description + ".\n" + getExitString() + getTasksInRoom() + getItemStringsInRoom() + getNPCInRoom();
+    }
+
+    public Item getItem(String name){
+        Item item = itemsInRoom.get(name);
+        return item;
+    }
+
+    public void removeItem(String name){
+        itemsInRoom.remove(name);
+
+    }
+
+    public String getItemStringsInRoom()
+    {
+        String temp = " ";
+        if(isItemsInRoom())
+        {
+            temp = "\n" + "Items:";
+            Set<String> items = itemsInRoom.keySet();
+            for (String itemsInRoom: items)
+            {
+                temp += " " + itemsInRoom;
+            }
+        }
+        return temp;
+    }
+
+    public boolean isItemsInRoom()
+    {
+        if(itemsInRoom.size() >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String getTasksInRoom()
+    {
+        String temp = " ";
+        if(isTasksInRoom())
+        {
+            temp = "\n" + "Tasks in the room:";
+            Set<String> tasks = tasksInRoom.keySet();
+            for(String tasksInRoom : tasks)
+            {
+                temp += " " + tasksInRoom;
+            }
+        }
+        return temp;
+    }
+
+    public void removeTaskFromRoom(String task)
+    {
+        tasksInRoom.remove(task);
+    }
+
+    public void setRoomImage(String pathName)
+    {
+        File file = new File(pathName);
+        roomImage = new Image(file.toURI().toString());
+    }
+
+    public void setRoomExit(int x, int y)
+    {
+        exitLocations[exitCounter][0] = x;
+        exitLocations[exitCounter][1] = y;
+    }
+
+    public boolean isWall(int x, int y)
+    {
+        boolean result = false;
+
+        for (int i = 0; i < boundaries.length; i++)
+        {
+            if(boundaries[i][0] == x)
+            {
+                if(boundaries[i][1] == y)
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isTasksInRoom()
+    {
+        if(tasksInRoom.size() >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String getNPCInRoom()
+    {
+        String temp = " ";
+        if(isNPCInRoom())
+        {
+            temp = "\n";
+            Set<String> NPCs = NPCsInRoom.keySet();
+            for (String NPCsInRoom: NPCs)
+            {
+                temp += " " + NPCsInRoom + " is in the Room.";
+            }
+
+        }
+        return temp;
+    }
+
+    public boolean isNPCInRoom()
+    {
+        if(NPCsInRoom.size() >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private String getExitString()
+    {
+        String returnString = "Exits:";
+        Set<String> keys = exits.keySet();
+        for(String exit : keys) {
+            returnString += " " + exit;
+        }
+        return returnString;
+    }
+
+    public Room getExit(String direction) 
+    {
+        return exits.get(direction);
+    }
+
+    public Task getTask(String name)
+    {
+        return tasksInRoom.get(name);
+    }
+
+    public void addItem(String itemName, Item item)
+    {
+        itemsInRoom.put(itemName, item);
+    }
+
+    public void addTask(String taskName,Task task)
+    {
+        tasksInRoom.put(taskName, task);
+    }
+
+    public void addNPC(String npcName, NPC npc)
+    {
+        NPCsInRoom.put(npcName, npc);
+    }
+}
+
--- Oceania/src/sample/Controller.java
+++ Oceania/src/sample/Controller.java
@@ -1,33 +1,117 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import Room.*;
+import javafx.scene.layout.Pane;
 
-public class Controller {
+public class Controller implements Initializable {
     public Label myLabel;
     public Button myButton;
     public AnchorPane myAnchorPane;
     public ImageView backgroundImage;
-    //public Image woods = "@../../backgroundTest.jpg";
+    public double width,height;
+    public Image woods,island;
+    public Pane pane1;
+    private Room town_square, shopping_street, currentRoom;
 
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        createRooms();
+        width = backgroundImage.getFitWidth();
+        height = backgroundImage.getFitHeight();
+        currentRoom = town_square;
+    }
+
+    private void createRooms()
+    {
+        town_square = new Room("at the town square",3);
+        shopping_street = new Room("at the shopping street",3);
+
+        town_square.setRoomImage("island.png");
+        town_square.setRoomExit(768,972);
+        town_square.setRoomExit(960,972);
+
+        shopping_street.setRoomImage("woods.jpg");
+    }
 
     public void changeText(ActionEvent actionEvent) {
         myLabel.setText("TESTY McTest");
     }
 
     public void handle(javafx.scene.input.KeyEvent keyEvent) {
-        double width = myAnchorPane.getWidth();
-        double height = myAnchorPane.getHeight();
         switch (keyEvent.getCode())
         {
-            case W: myLabel.setText("W"); myLabel.setLayoutY(myLabel.getLayoutY() - height/10); /*backgroundImage.setImage("@../../backgroundTest.jpg")*/;
-            case A: myLabel.setText("A"); myLabel.setLayoutX(myLabel.getLayoutX() - width/10); break;
-            case S: myLabel.setText("S"); myLabel.setLayoutY(myLabel.getLayoutY() + height/10); break;
-            case D: myLabel.setText("D"); myLabel.setLayoutX(myLabel.getLayoutX() + width/10); break;
+            case W: moveUp(currentRoom);   break;
+            case A: moveLeft(currentRoom); break;
+            case S: moveDown(currentRoom); break;
+            case D: moveRight(currentRoom);break;
         }
+    }
+
+    public void moveUp(Room currentRoom)
+    {
+        int x = (int) myLabel.getLayoutX();
+        int y = (int) myLabel.getLayoutY();
+        if(!currentRoom.isWall(x,y- (int) height/10))
+        {
+            myLabel.setLayoutY(myLabel.getLayoutY() - height/10);
+            System.out.println(myLabel.getLayoutX() + ", " + myLabel.getLayoutY());
+        }
+
+    }
+    public void moveDown(Room currentRoom)
+    {
+        int x = (int) myLabel.getLayoutX();
+        int y = (int) myLabel.getLayoutY();
+        if(!currentRoom.isWall(x,y + (int) height/10))
+        {
+            myLabel.setLayoutY(myLabel.getLayoutY() + height/10);
+            System.out.println(myLabel.getLayoutX() + ", " + myLabel.getLayoutY());
+        }
+    }
+    public void moveLeft(Room currentRoom)
+    {
+        int x = (int) myLabel.getLayoutX();
+        int y = (int) myLabel.getLayoutY();
+        if(!currentRoom.isWall(x - (int) width/10, y))
+        {
+            myLabel.setLayoutX(myLabel.getLayoutX() - width/10);
+            System.out.println(myLabel.getLayoutX() + ", " + myLabel.getLayoutY());
+        }
+    }
+    public void moveRight(Room currentRoom)
+    {
+        int x = (int) myLabel.getLayoutX();
+        int y = (int) myLabel.getLayoutY();
+        if(!currentRoom.isWall(x + (int) width/10, y))
+        {
+            myLabel.setLayoutX(myLabel.getLayoutX() + width/10);
+            System.out.println(myLabel.getLayoutX() + ", " + myLabel.getLayoutY());
+        }
+    }
+
+    public void changeColor(MouseEvent mouseEvent)
+    {
+        Pane tempPane = (Pane) mouseEvent.getTarget();
+        tempPane.setStyle("-fx-background-color: #ff0000;");
+    }
+
+    public void changeColorNormal(MouseEvent mouseEvent)
+    {
+       Pane tempPane = (Pane) mouseEvent.getTarget();
+       tempPane.setStyle("-fx-background-color: #ffffff;");
     }
 }
